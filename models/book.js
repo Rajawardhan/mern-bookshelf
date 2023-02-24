62bee24f8d3e792a7e6083ff9e7ff96e117c8a4d
Rajavardhan <vardhan.410@gmail.com>
2023-02-24 10:44:36 +0530

    Create book.js

---
+++ models/book.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    authors: {
+        type: [String],
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    }
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
